9864d1e3cafd785088fbe43301a7cf2ddaeacc0b
rhett <rhettapples@googleemailservice.com>
2024-07-22 18:36:32 -0400

    d

--- rhetts-install.sh
+++ rhetts-install.sh
@@ -14,7 +14,7 @@ cd /usr/bin
 curl https://getmic.ro | sudo sh
 
 
-sudo apt install git
+sudo apt install git -y
 
 cd /home/$u
 
@@ -28,7 +28,7 @@ mv -v /home/$u/micro-all/* /home/$u/.config/micro
 
 
 
-sudo apt install kicad
+sudo apt install kicad -y
 
 sudo rm /etc/apt/preferences.d/nosnap.pref
 
@@ -36,7 +36,7 @@ sudo apt update
 
 sudo apt install snapd
 
-sudo snap install --edge freecad
+sudo snap install --edge freecad -y
 
 
 
@@ -44,7 +44,7 @@ sudo snap install --edge freecad
 
 cd /home/$u
 
-sudo apt install git python3-setuptools gettext
+sudo apt install git python3-setuptools gettext -y
 git clone https://github.com/sezanzeb/input-remapper.git
 cd input-remapper && ./scripts/build.sh
 sudo apt install -f ./dist/input-remapper-2.0.1.deb
@@ -53,6 +53,6 @@ sudo apt install -f ./dist/input-remapper-2.0.1.deb
 #inkscape
 
 sudo apt-get update
-sudo apt-get install inkscape
+sudo apt-get install inkscape -y
 
 cd /home/$u
